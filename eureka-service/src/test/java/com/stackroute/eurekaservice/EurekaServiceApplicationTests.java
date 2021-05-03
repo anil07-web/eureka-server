5fbdf044ced80a2444b1528a3826c893af40d8a6
anilkumar75001900 <anilkumar75001900@gmail.com>
2021-05-03 16:58:31 +0530

    initial commit

---
+++ eureka-service/src/test/java/com/stackroute/eurekaservice/EurekaServiceApplicationTests.java
@@ -0,0 +1,15 @@
+package com.stackroute.eurekaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+
+@SpringBootTest
+class EurekaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
